4e9c399911fb43f089f38a1897b81fdc25f04b31
Kiss György <kissgyorgy@me.com>
2014-10-22 21:32:51 +0200

    Optimized algorithm:
    less arithmetical operations needed; one addition and a substraction of
    single element. Much faster with huge subset numbers, because don't
    need to recalculate the point number of element average, just the average
    of the first number of the set and the next number after the set.

--- moving_average.py
+++ moving_average.py
@@ -2,8 +2,13 @@ def moving_average(dataset, points):
     ma = []
     data_len = len(dataset)
 
-    for current in range(data_len - points + 1):
-        average = sum(dataset[current:current+points]) / points
+    average = sum(dataset[:points]) / points
+    ma.append(average)
+    for first_of_set in range(data_len - points):
+        next = first_of_set + points
+        first_of_set_average = dataset[first_of_set] / points
+        next_average = dataset[next] / points
+        average = average - first_of_set_average + next_average
         ma.append(average)
 
     return ma
