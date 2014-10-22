51e9262ff273db870310453797dbeb48eefd4df7
Kiss György <kissgyorgy@me.com>
2014-10-22 22:39:38 +0200

    Implement save functionality with POST request

--- logcollector/__init__.py
+++ logcollector/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
+from .models import DataPoint
 
 
 app = Flask(__name__)
@@ -7,6 +8,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../logcollector.db'
 db = SQLAlchemy(app)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/new", methods=['POST'])
+def collect():
+    new_data = DataPoint(request.form['timestamp'], request.form['dim1'],
+                         request.form['dim2'], request.form['value']
+                         )
+    db.session.add(new_data)
+    db.session.commit()
+
+    return jsonify(id=new_data.id, timestamp=request.form['timestamp'],
+                   dim1=new_data.dim1, dim2=new_data.dim2), 201
