e211cc0406cb6a3269ff34384a35fbbe578500d5
Kiss György <kissgyorgy@me.com>
2014-10-22 21:38:48 +0200

    More complicated dataset, rounding to 5 decimal numbers

--- moving_average.py
+++ moving_average.py
@@ -3,12 +3,12 @@ def moving_average(dataset, points):
     data_len = len(dataset)
 
     average = sum(dataset[:points]) / points
-    ma.append(average)
+    ma.append(round(average, 5))
     for first_of_set in range(data_len - points):
         next = first_of_set + points
         first_of_set_average = dataset[first_of_set] / points
         next_average = dataset[next] / points
         average = average - first_of_set_average + next_average
-        ma.append(average)
+        ma.append(round(average, 5))
 
     return ma
--- tests/test_moving_average.py
+++ tests/test_moving_average.py
@@ -8,3 +8,9 @@ def test_zeroes_only():
 
 def test_simple_dataset():
     assert moving_average([0, 1, 2], 2) == [0.5, 1.5]
+
+
+def test_complicated_dataset():
+    dataset = [0.54, 0.21, 0.71, 0.17, 0.94, 0.7, 0.67, 1.0, 0.75, 0.63]
+    results = [0.48667, 0.36333, 0.60667, 0.60333, 0.77, 0.79, 0.80667, 0.79333]
+    assert moving_average(dataset, 3) == results
