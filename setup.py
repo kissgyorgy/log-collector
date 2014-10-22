a0182c57f411e7ac7e03e462330ddeb39e178c02
Kiss György <kissgyorgy@me.com>
2014-10-22 19:49:35 +0200

    Basic folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name = 'logcollector',
+    version = '0.1.0',
+    author = 'Kiss György',
+    description = 'Log collector task for Job application',
+    py_modules = ['logcollector', 'moving_average']
+)
