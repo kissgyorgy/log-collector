0befbc282cf6bc7c6e198b2bba2f8c720696cfdc
Kiss György <kissgyorgy@me.com>
2014-10-22 20:39:43 +0200

    Moving average with simple dataset

--- moving_average.py
+++ moving_average.py
@@ -1,2 +1,9 @@
 def moving_average(dataset, points):
-    return [0]
+    ma = []
+    data_len = len(dataset)
+
+    for current in range(data_len - points + 1):
+        average = sum(dataset[current:current+points]) / points
+        ma.append(average)
+
+    return ma
--- tests/test_moving_average.py
+++ tests/test_moving_average.py
@@ -4,3 +4,7 @@ import pytest
 
 def test_zeroes_only():
     assert moving_average([0, 0, 0], 2) == [0, 0]
+
+
+def test_simple_dataset():
+    assert moving_average([0, 1, 2], 2) == [0.5, 1.5]
