c9f0782d0a2c20b08d5fc8f1c607b802a358a18b
Kiss György <kissgyorgy@me.com>
2014-10-22 20:21:12 +0200

    Simplest possible moving average

---
+++ moving_average.py
@@ -0,0 +1,2 @@
+def moving_average(dataset, points):
+    return [0]
--- tests/test_moving_average.py
+++ tests/test_moving_average.py
@@ -1,2 +1,6 @@
+from moving_average import moving_average
 import pytest
 
+
+def test_zeroes_only():
+    assert moving_average([0, 0, 0], 2) == [0, 0]
