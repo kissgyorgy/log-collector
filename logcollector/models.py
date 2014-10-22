780c74f956e56575bb095896d599cd38664d62c5
Kiss György <kissgyorgy@me.com>
2014-10-22 22:38:33 +0200

    SQLite doesn't support timestamp, so use DateTime instead,
    convert timestamp to DateTime at object instantiation time and store that in the database.

--- logcollector/models.py
+++ logcollector/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, Float
-from sqlalchemy.types import TIMESTAMP
-from sqlalchemy.orm import declarative_base
+from datetime import datetime
+from sqlalchemy import Column, Integer, Float, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
@@ -10,13 +10,13 @@ class DataPoint(Base):
     __tablename__ = 'datapoint'
 
     id = Column(Integer, primary_key=True)
-    timestamp = Column(TIMESTAMP)
+    timestamp = Column(DateTime)
     dim1 = Column(Integer)
     dim2 = Column(Integer)
     value = Column(Float)
 
     def __init__(self, timestamp, dim1, dim2, value):
-        self.timestamp = timestamp
-        self.dim1 = dim1
-        self.dim2 = dim2
-        self.value = value
+        self.timestamp = datetime.fromtimestamp(int(timestamp))
+        self.dim1 = int(dim1)
+        self.dim2 = int(dim2)
+        self.value = float(value)
