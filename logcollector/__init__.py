4ca10387e4aad82c3c283d39b8dee56e09acb940
Kiss György <kissgyorgy@me.com>
2014-10-23 09:39:30 +0200

    mean aggregation functionality implementation and test

--- logcollector/__init__.py
+++ logcollector/__init__.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import statistics
 from flask import Flask, request, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 from .models import DataPoint
@@ -18,3 +20,18 @@ def collect():
 
     return jsonify(id=new_data.id, timestamp=request.form['timestamp'],
                    dim1=new_data.dim1, dim2=new_data.dim2), 201
+
+
+@app.route('/mean/<first>/<second>')
+def mean(first, second):
+    first_date = datetime.fromtimestamp(float(first))
+    second_date = datetime.fromtimestamp(float(second))
+
+    data_points = db.session\
+                    .query(DataPoint.value)\
+                    .filter(first_date < DataPoint.timestamp,
+                            DataPoint.timestamp < second_date)\
+                    .all()
+
+    dataset = [d[0] for d in data_points]
+    return jsonify(mean=statistics.mean(dataset))
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,4 +1,6 @@
+import json
 import pytest
+import datetime
 from flask.ext.sqlalchemy import SQLAlchemy
 from logcollector import app
 from logcollector.models import Base, DataPoint
@@ -26,10 +28,19 @@ def test_app():
     return app.test_client()
 
 
+def to_dict(response):
+    return json.loads(response.data.decode())
+
+
 def test_can_post_new_data(test_app, db):
     assert db.session.query(DataPoint).count() == 5
     test_app.post('/new', data={'timestamp': '1000000060', 'dim1': 6,
                                 'dim2': 3, 'value': 60})
     assert db.session.query(DataPoint).count() == 6
     assert db.session.query(DataPoint).get(6).value == 60
+
+
+def test_mean(test_app, db):
+    res = test_app.get('/mean/1000000000/1000000050')
+    assert to_dict(res) == {'mean': 25.0}
 
