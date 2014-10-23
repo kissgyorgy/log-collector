0cd5d4cf9c4d26f691ac7f7f185fe3415ded1c7e
Kiss György <kissgyorgy@me.com>
2014-10-24 00:04:58 +0200

    Missing requests dependency

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     description = 'Log collector task for Job application',
     modules = find_packages(),
     entry_points = {'console_scripts': ['logcollector = logcollector.commands:logcollector']},
-    install_requires = ['flask', 'sqlalchemy', 'flask-sqlalchemy', 'click']
+    install_requires = ['flask', 'sqlalchemy', 'flask-sqlalchemy', 'click', 'requests']
 )
